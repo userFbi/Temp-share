acd344af4d80621eb69bd35358aacf889a987278
userFbi <pawartushar2024.katargam@gmail.com>
2026-02-03 09:22:56 +0530

    file renamed and modified

--- Temp-react-layout/src/App.jsx
+++ Temp-react-layout/src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
+import "./star.css"
+
 const queryClient = new QueryClient();
 
 const App = () => (
